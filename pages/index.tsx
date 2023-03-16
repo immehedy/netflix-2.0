617d08c8710d309a03a32e2b8c9d14e37b96b0dc
mehedy hassan <dev.mehedy.hassan@gmail.com>
2023-03-17 01:43:33 +0600

    project scafolded

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <TabHeader title="Netflix 2.0" meta="Netflix web clone app"/>
-      <div className='text-xl text-green-400'>hello</div>
+      <div className='text-xl text-green-400'>Netflix 2.0</div>
     </>
   )
 }
