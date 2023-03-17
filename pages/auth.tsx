866b54792ad08ab16f27b4562b148202b6a6f6bb
mehedy hassan <dev.mehedy.hassan@gmail.com>
2023-03-18 03:09:31 +0600

    logo and hero image added

---
+++ pages/auth.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const auth = () => {
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpeg')] bg-no-repeat bg-center bg-fixed bg-cover">
+        <div className="bg-black h-full w-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+            <img src='/images/logo.png' alt='logo' className='h-12'/>
+        </nav>
+        </div>
+    </div>
+  )
+}
+
+export default auth
