9fe4fa38a32ddd22bc296c9a0a16600b4b94a6e8
mehedy hassan <dev.mehedy.hassan@gmail.com>
2023-03-18 22:44:06 +0600

    auth UI design completed

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,15 +1,61 @@
-import React from 'react'
+import Input from "@/components/reusable/Input";
+import React, { useCallback, useState } from "react";
 
-const auth = () => {
+const Auth = () => {
+    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [variant, setVariant] = useState('login');
+
+    const changeVariant = useCallback(() => {
+        setVariant((currentVarient) => currentVarient === 'login' ? 'register' : 'login');
+    }, [])
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpeg')] bg-no-repeat bg-center bg-fixed bg-cover">
-        <div className="bg-black h-full w-full lg:bg-opacity-50">
+      <div className="bg-black h-full w-full lg:bg-opacity-50">
         <nav className="px-12 py-5">
-            <img src='/images/logo.png' alt='logo' className='h-12'/>
+          <img src="/images/logo.png" alt="logo" className="h-12" />
         </nav>
+        <div className="flex justify-center">
+          <div className="bg-black bg-opacity-70 p-16 self-center lg:w-2/5 lg:max-w-md w-full rounded-md">
+            <h2 className="text-white font-semibold text-4xl mb-4">{variant === 'login' ? 'Sign in' : 'Register'}</h2>
+            <div className="flex flex-col gap-4">
+              {
+                variant === 'register' && 
+                <Input
+                label="Your name"
+                id="name"
+                type="text"
+                onChange={(ev : any) => {setUserName(ev.target.value)}}
+                value={userName}
+              />
+              }
+              <Input
+                label="Your email"
+                id="email"
+                type="email"
+                onChange={(ev : any) => {setEmail(ev.target.value)}}
+                value={email}
+              />
+              <Input
+                label="Password"
+                id="password"
+                type="password"
+                onChange={(ev : any) => {setPassword(ev.target.value)}}
+                value={password}
+              />
+            </div>
+            <button className="bg-red-600 text-white py-3 mt-10 rounded-md w-full hover:bg-red-700 transition">
+                {variant === 'login' ? 'Login' : 'Sign up'}
+            </button>
+            <p className="text-neutral-500 mt-12 text-center"> {variant === 'login' ? 'New to netflix? ' : 'Already have an account? '} 
+                <span className="text-white ml-1 cursor-pointer hover:underline" onClick={changeVariant}>{variant === 'login' ? 'create an account' : 'login'}</span>
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default auth
+export default Auth;
